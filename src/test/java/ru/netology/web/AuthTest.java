a6b7a34de3e8493f60b0529b01d6b94b04541c0d
“Natalia <fa.nat.ka911@gmail.com>
2026-04-21 14:24:46 +0700

    all tests are new

--- src/test/java/ru/netology/web/AuthTest.java
+++ src/test/java/ru/netology/web/AuthTest.java
@@ -1,94 +1,88 @@
 package ru.netology.web;
 
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.LogDetail;
-import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-import static ru.netology.web.RegistrationDto.STATUS_ACTIVE;
-import static ru.netology.web.RegistrationDto.STATUS_BLOCKED;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.web.DataGenerator.*;
+import static ru.netology.web.DataGenerator.Registration.getRegisteredUser;
+import static ru.netology.web.DataGenerator.Registration.getUser;
 
-// спецификация нужна для того, чтобы переиспользовать настройки в разных запросах
 class AuthTest {
 
-    public static final String CREATE_USER_ENDPOINT = "/api/system/users";
-
-    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
-            .setBaseUri("http://localhost")
-            .setPort(9999)
-            .setAccept(ContentType.JSON)
-            .setContentType(ContentType.JSON)
-            .log(LogDetail.ALL)
-            .build();
-
-    @Test
-    void testSuccessCreateActiveUser() {
-        RegistrationDto user = AuthTestDataGenerator.generateUser("en", STATUS_ACTIVE);
-        checkCreateUserRequest(user, 200);
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
     }
 
     @Test
-    void testSuccessCreateBlockedUser() {
-        RegistrationDto user = AuthTestDataGenerator.generateUser("en", STATUS_BLOCKED);
-        checkCreateUserRequest(user, 200);
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+        var registeredUser = getRegisteredUser("active");
+             // данными зарегистрированного активного пользователя, для заполнения полей формы используйте
+        // пользователя registeredUser
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("h2").shouldBe(Condition.visible).shouldHave(Condition.text("Личный кабинет"));
     }
 
     @Test
-    void testSuccessCreateActiveUserTwiceSameLogin() {
-        String login = AuthTestDataGenerator.generateLogin("en");
-
-        RegistrationDto user = AuthTestDataGenerator.generateUser("en", login, STATUS_ACTIVE);
-        checkCreateUserRequest(user, 200);
-
-        RegistrationDto user2 = AuthTestDataGenerator.generateUser("en", login, STATUS_ACTIVE);
-        checkCreateUserRequest(user2, 200);
+    @DisplayName("Should get error message if login with not registered user")
+    void shouldGetErrorIfNotRegisteredUser() {
+        var notRegisteredUser = getUser("active");
+        //  незарегистрированного пользователя, для заполнения полей формы используйте пользователя notRegisteredUser
+        $("[data-test-id='login'] input").setValue(notRegisteredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(notRegisteredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification']")
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"));
     }
 
     @Test
-    void testSuccessCreateBlockedUserTwiceSameLogin() {
-        String login = AuthTestDataGenerator.generateLogin("en");
-
-        RegistrationDto user = AuthTestDataGenerator.generateUser("en", login, STATUS_BLOCKED);
-        checkCreateUserRequest(user, 200);
-
-        RegistrationDto user2 = AuthTestDataGenerator.generateUser("en", login, STATUS_BLOCKED);
-        checkCreateUserRequest(user2, 200);
+    @DisplayName("Should get error message if login with blocked registered user")
+    void shouldGetErrorIfBlockedUser() {
+        var blockedUser = getRegisteredUser("blocked");
+        //  заблокированного пользователя, для заполнения полей формы используйте пользователя blockedUser
+        $("[data-test-id='login'] input").setValue(blockedUser.getLogin());
+        $("[data-test-id='password'] input").setValue(blockedUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification']")
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Ошибка! Пользователь заблокирован"));
     }
 
     @Test
-    void testSuccessCreateUserTwiceSameLoginChangeStatus() {
-        String login = AuthTestDataGenerator.generateLogin("en");
-
-        RegistrationDto user = AuthTestDataGenerator.generateUser("en", login, STATUS_BLOCKED);
-        checkCreateUserRequest(user, 200);
-
-        RegistrationDto user2 = AuthTestDataGenerator.generateUser("en", login, STATUS_ACTIVE);
-        checkCreateUserRequest(user2, 200);
+    @DisplayName("Should get error message if login with wrong login")
+    void shouldGetErrorIfWrongLogin() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongLogin = getRandomLogin();
+        //  логином, для заполнения поля формы "Логин" используйте переменную wrongLogin,
+        //  "Пароль" - пользователя registeredUser
+        $("[data-test-id='login'] input").setValue(wrongLogin);
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification']")
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"));
     }
 
     @Test
-    void testSuccessCreateUserTwiceSameLoginChangeStatus2() {
-        String login = AuthTestDataGenerator.generateLogin("en");
-
-        RegistrationDto user = AuthTestDataGenerator.generateUser("en", login, STATUS_ACTIVE);
-        checkCreateUserRequest(user, 200);
-
-        RegistrationDto user2 = AuthTestDataGenerator.generateUser("en", login, STATUS_BLOCKED);
-        checkCreateUserRequest(user2, 200);
+    @DisplayName("Should get error message if login with wrong password")
+    void shouldGetErrorIfWrongPassword() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongPassword = getRandomPassword();
+        //  паролем, для заполнения поля формы "Логин" используйте пользователя registeredUser,
+        //  "Пароль" - переменную wrongPassword
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(wrongPassword);
+        $("[data-test-id='action-login']").click();
+        $("[data-test-id='error-notification']")
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"));
     }
-
-    private void checkCreateUserRequest(RegistrationDto userDto, int expectedStatus) {
-        given() // "дано"
-                .spec(requestSpec) // указываем, какую спецификацию используем
-                // передаём в теле объект, который будет преобразован в JSON
-                .body(userDto)
-                .when() // "когда"
-                // на какой путь относительно BaseUri отправляем запрос
-                .post(CREATE_USER_ENDPOINT)
-                .then() // "тогда ожидаем"
-                .statusCode(expectedStatus); // код 200 OK
-    }
-
 }
